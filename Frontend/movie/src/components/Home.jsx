baa4736481ad0daf26f711be3db90ea38575ff88
W2-80477 <satyamkumar.sk786@gmail.com>
2024-02-21 20:51:02 +0530

    home success

--- Frontend/movie/src/components/Home.jsx
+++ Frontend/movie/src/components/Home.jsx
@@ -35,22 +35,19 @@ function Home() {
           <div className="carousel-item active">
             <img src={AnimalLandscape} className="d-block w-100" alt="animal" />
             <div className="carousel-caption d-none d-md-block">
-              {/* <h5>First slide label</h5>
-              <p>Some representative placeholder content for the second slide.</p> */}
+             
             </div>
           </div>
           <div className="carousel-item">
             <img src={HanumanLandscape} className="d-block w-100" alt="landscapeHanuman" />
             <div className="carousel-caption d-none d-md-block">
-              {/* <h5>Second slide label</h5>
-              <p>Some representative placeholder content for the second slide.</p> */}
+             
             </div>
           </div>
           <div className="carousel-item">
             <img src={SalaarLandscape} className="d-block w-100" alt="landscapeSalaar" />
             <div className="carousel-caption d-none d-md-block">
-              {/* <h5>Third slide label</h5>
-              <p>Some representative placeholder content for the third slide.</p> */}
+            
             </div>
           </div>
         </div>
